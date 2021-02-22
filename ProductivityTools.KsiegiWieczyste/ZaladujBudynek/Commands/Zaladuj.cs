306c224d185711f11bd830dc347dec02f4ce1be9
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-02-22 17:07:47 +0100

    zaladuj budynek

---
+++ ProductivityTools.KsiegiWieczyste/ZaladujBudynek/Commands/Zaladuj.cs
@@ -0,0 +1,20 @@
+﻿using ProductivityTools.KsiegiWieczyste.NewFolder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.KsiegiWieczyste.ZaladujBudynek.Commands
+{
+
+    public class Zaladuj : PSCmdlet.PSCommandPT<ZaladujBudynekCmdlet>
+    {
+        public Zaladuj(ZaladujBudynekCmdlet cmdletType) : base(cmdletType) { }
+
+        protected override bool Condition => true;
+
+        protected override void Invoke()
+        {
+            Console.WriteLine("Hello from zalduj budynek");
+        }
+    }
+}
---
+++ ProductivityTools.KsiegiWieczyste/ZaladujBudynek/ZaladujBudynekCmdlet.cs
@@ -0,0 +1,23 @@
+﻿using ProductivityTools.KsiegiWieczyste.ZaladujBudynek.Commands;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace ProductivityTools.KsiegiWieczyste.NewFolder
+{
+    [Cmdlet("Zaladuj", "Budynek")]
+    public class ZaladujBudynekCmdlet : PSCmdlet.PSCmdletPT
+    {
+        public ZaladujBudynekCmdlet()
+        {
+        }
+
+        protected override void ProcessRecord()
+        {
+            this.AddCommand(new Zaladuj(this));
+            this.ProcessCommands();
+            base.ProcessRecord();
+        }
+    }
+}
